a3b99e814e928f5dfed2d502b6290e3b121d546b
Doyoon Kim <asfrom30@gmail.com>
2019-11-26 00:35:04 +0900

    on during fix home menu bug

--- _includes/home/home.js
+++ _includes/home/home.js
@@ -1,4 +1,26 @@
+function test(event) {
+  event.preventDefault();
+
+  const $_node = $(event.currentTarget);
+  const highlightTarget = $_node.find(".home-menu__collapse-title");
+  const collapseTarget = $_node.find(".home-menu__collapse-list");
+
+  if (highlightTarget.hasClass("highlight")) {
+    // collapse
+    highlightTarget.removeClass("highlight");
+    collapseTarget.hide("slow");
+  } else {
+    // show
+    highlightTarget.addClass("highlight");
+    collapseTarget.show("slow");
+  }
+
+  console.log(highlightTarget);
+  console.log(collapseTarget);
+}
+
 function toggleCollapseList(containerClassName, event) {
+  event.preventDefault();
   console.log(containerClassName);
   const $_target = $(event.target);
   const flag = $_target.hasClass("highlight");
