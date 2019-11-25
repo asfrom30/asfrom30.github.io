75b2b7427110ab126520fd109cfea4217c8fa962
Doyoon Kim <asfrom30@gmail.com>
2019-11-26 00:01:35 +0900

    apply font

--- _includes/home/home.js
+++ _includes/home/home.js
@@ -3,15 +3,15 @@ function toggleCollapseList(containerClassName, event) {
   const $_target = $(event.target);
   const flag = $_target.hasClass("highlight");
 
+  console.log(flag);
+
   // highlight clicked title
   $(".highlight").removeClass("highlight");
 
   if (flag) {
     $(`.${containerClassName} .home-menu__collapse-list`).hide("slow");
   } else {
-    $(`.${containerClassName} .home-menu__collapse-title`).addClass(
-      "highlight"
-    );
+    $(`.${containerClassName} .home-menu__collapse-title`).addClass("highlight");
     $(`.home-menu__collapse-list`).hide("slow"); // collapse all
     $(`.${containerClassName} .home-menu__collapse-list`).toggle("slow");
   }
