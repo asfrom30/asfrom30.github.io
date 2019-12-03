e468a081dc6482496904afd9da5359aecbd1cd45
Doyoon Kim <asfrom30@gmail.com>
2019-12-03 21:26:05 +0900

    191203

---
+++ _drafts/2019/still-code/instant/Parent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>Enable Left : {{enableLeft}}</div>
+    <div>obj : {{obj}}</div>
+    <!-- <child v-bind:enableLeft.sync="enableLeft"></child> -->
+    <child v-bind.sync="obj"></child>
+    <button @click="obj.enableLeft = true">Parent button</button>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue';
+/* eslint-disable  */
+export default {
+  components: { Child },
+  data() {
+    return {
+      obj: {
+        enableLeft: undefined,
+      },
+      enableLeft: undefined,
+    };
+  },
+  methods: {
+    listen(value) {
+      console.log(value);
+    },
+  },
+};
+/* eslint-enable  */
+</script>
+
+<style>
+</style>
